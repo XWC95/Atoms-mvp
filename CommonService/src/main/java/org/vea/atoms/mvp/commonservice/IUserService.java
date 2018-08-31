224e9c7d6be6f56e7c33223e901b8f15540d93d2
xwc520 <m13052509803@163.com>
2018-08-31 13:27:47 +0800

    Add module CommonService

---
+++ CommonService/src/main/java/org/vea/atoms/mvp/commonservice/IUserService.java
@@ -0,0 +1,24 @@
+package org.vea.atoms.mvp.commonservice;
+
+import com.alibaba.android.arouter.facade.template.IProvider;
+
+/**
+ * ================================================
+ * <p>
+ * Created by Vea on 2018/8/30.
+ * ================================================
+ */
+public interface IUserService extends IProvider {
+
+    String getAvatarUrl();
+
+    int getId();
+
+    String getLogin();
+
+    void setAvatar_url(String avatar_url);
+
+    void setId(int id);
+
+    void setLogin(String login);
+}
